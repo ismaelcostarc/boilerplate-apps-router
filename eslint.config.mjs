44f682f19d6ec2cd5e6da870b1188d04643d094c
Ismael Costa <ismaelac96@icloud.com>
2024-10-03 07:54:51 -0300

    build: add prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
@@ -10,4 +11,5 @@ export default [
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   pluginReact.configs.flat["jsx-runtime"],
+  eslintConfigPrettier,
 ];
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Script from "next/script";
 
 export default function Home() {
-  const foo = 'bar'
+  const foo = "bar";
   return (
     <div className={styles.page}>
       <main className={styles.main}>
