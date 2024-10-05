56b707eb031e41324c3af0ae8376f298c8ed92b1
Ismael Costa <ismaelac96@icloud.com>
2024-10-05 06:52:48 -0300

    test jest

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -5,9 +5,7 @@ describe("<Main />", () => {
   it("should render the heading", () => {
     const { container } = render(<Main />);
 
-    expect(
-      screen.getByRole("heading", { name: /asdasdqweqwe/i }),
-    ).toBeInTheDocument();
+    expect(screen.getByRole("heading", { name: /React/i })).toBeInTheDocument();
 
     expect(container.firstChild).toMatchSnapshot();
   });
